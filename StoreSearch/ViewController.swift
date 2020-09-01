f4430f97ad745dd72e44a3537d22ead3b0a8e4e3
ufukcanli <iletisim@ufukcanli.com>
2020-09-01 22:32:33 +0300

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Ufuk Canlı on 1.09.2020.
+//  Copyright © 2020 Ufuk Canlı. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
