05ccfb4afebde8c4bac48fb2707fdb9de62a8cdd
ufukcanli <iletisim@ufukcanli.com>
2020-09-02 01:15:12 +0300

    Add a search bar and table view. The search puts fake results in the table for now

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,12 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Ufuk Canlı on 2.09.2020.
+//  Copyright © 2020 Ufuk Canlı. All rights reserved.
+//
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+}
